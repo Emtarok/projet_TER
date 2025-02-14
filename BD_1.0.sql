0fd22ccda1572d81314e1718b93acd9f3fdfdd02
Emtarok <79385933+Emtarok@users.noreply.github.com>
2025-02-14 14:43:41 +0100

    maj de la bd : j'ai modifié le nom des attributs/tables pour correspondre à la version de la bd précédente

--- BD_1.0.sql
+++ BD_1.0.sql
@@ -1,32 +1,43 @@
 CREATE DATABASE postit;
 
--- Table des dimensions
-CREATE TABLE Utilisateur (
-    id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE utilisateurs (
+    idutilisateur INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100) NOT NULL,
     prenom VARCHAR(100) NOT NULL,
     pseudo VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    mot_de_passe VARCHAR(100) NOT NULL
+    date_naissance DATE NOT NULL,
+    motdepasse VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE PostIt (
-    id_postit INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE postit (
+    idpostit INT AUTO_INCREMENT PRIMARY KEY,
     titre VARCHAR(200) NOT NULL,
     contenu TEXT NOT NULL,
-    date_creation DATETIME NOT NULL,
-    id_utilisateur INT NOT NULL,
-    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateur(id_utilisateur) ON DELETE CASCADE
+    date DATETIME NOT NULL,
+    idutilisateurs INT NOT NULL,
+    FOREIGN KEY (idutilisateur) REFERENCES utilisateur(idutilisateur) ON DELETE CASCADE
 );
 
--- Table des faits : Fait_PostIt (centralise post-its et partages)
-CREATE TABLE Fait_PostIt (
-    id_fait INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE faits (
+    idfaits INT AUTO_INCREMENT PRIMARY KEY,
     id_postit INT NOT NULL,
-    id_utilisateur_auteur INT NOT NULL,
+    id_utilisateur INT NOT NULL,
     id_utilisateur_partage INT NOT NULL,
-    FOREIGN KEY (id_postit) REFERENCES PostIt(id_postit) ON DELETE CASCADE,
-    FOREIGN KEY (id_utilisateur_auteur) REFERENCES Utilisateur(id_utilisateur) ON DELETE CASCADE,
-    FOREIGN KEY (id_utilisateur_partage) REFERENCES Utilisateur(id_utilisateur) ON DELETE CASCADE,
-    UNIQUE (id_postit, id_utilisateur_partage) -- Évite les doublons de partage
+    FOREIGN KEY (id_postit) REFERENCES postit(idpostit) ON DELETE CASCADE,
+    FOREIGN KEY (id_uutilisateur) REFERENCES utilisateur(idutilisateur) ON DELETE CASCADE,
+    FOREIGN KEY (id_utilisateur_partage) REFERENCES utilisateur(idutilisateur) ON DELETE CASCADE,
+    UNIQUE (id_postit, id_utilisateur_partage)
 );
+
+INSERT INTO utilisateurs (nom, prenom, pseudo, email, date_naissance, motdepasse)
+VALUES ('Vuillot', 'Kevin-Swami', 'kevin','01/01/2002', 'kevin@gmail.com', '');
+
+INSERT INTO utilisateurs (nom, prenom, pseudo, email, date_naissance, motdepasse)
+VALUES ('Legrand', 'Sophie', 'sophie','01/01/2002', 'sophie@gmail.com', '');
+
+INSERT INTO utilisateurs (nom, prenom, pseudo, email, date_naissance, motdepasse)
+VALUES ('M''Baye', 'Fatimata', 'fatimata','01/01/2002', 'fatimata@gmail.com', '');
+
+INSERT INTO utilisateurs (nom, prenom, pseudo, email, date_naissance, motdepasse)
+VALUES ('Rakotoson', 'Hasimbola', 'hasimbola','01/01/2002', 'hasimbola@gmail.com', '');
--- README.md
+++ README.md
@@ -4,21 +4,23 @@ Répértoire pour le projet de TER
 Date limite : 6 mars 12h00
 
 Outils de gestion de projet : Trello, Bizagi, Excel
-
+Outils de développement : Git, php, html/css, Javascript, BootStrap, Jquery
 
 Base de donnée :
 
 Dans une architecture en étoile, les tables de dimension servent à enrichir la table des faits avec des informations détaillées. Ici, la question est de savoir si les post-its partagés doivent être dans une table distincte ou s'ils restent dans la table PostIt.
-
 Un post-it partagé est un post-it existant, simplement visible par un autre utilisateur.
 Il n'y a pas de transformation du post-it lors du partage.
 La table PostIt contient déjà les informations descriptives du post-it (titre, contenu, auteur).
-
 Il n'est pas nécessaire de créer une table de dimension spécifique pour les post-its partagés.
 L'information du partage est déjà bien gérée par la table des faits Fait_PostIt, qui établit la relation entre un post-it, son auteur et l'utilisateur avec qui il est partagé.
 
-Le Partage d'un post-it (possibilité):
+UNIQUE (id_postit, id_utilisateur_partage) permet d'éviter les doublons dans la table des faits_postit
+
+Le Partage d'un post-it (possibilité): 
 
 - jquery, utilisation de l'auto-complétion
 - html, liste des tous les utilisateurs et on séléctionne le bon
 - javascript, utilisation de filtre similaire au projet de boutique web
+
+Pour choisir l'utilisateur avec lequel on partage le post-it on a choisi jquery car c'est celui le plus utilisé dans le monde l'entreprise et c'est le plus pratique pour l'utilisateur. On utilisera le nom et le prenom pour faire la recherche.
