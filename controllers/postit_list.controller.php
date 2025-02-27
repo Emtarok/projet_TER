a98643d87175142cbdec062fdd428961ad5c3c5a
Sophie-Redkali <sophie.legrand3391@gmail.com>
2025-02-27 11:14:57 +0100

    modif postit_list_controller

--- controllers/postit_list.controller.php
+++ controllers/postit_list.controller.php
@@ -5,8 +5,15 @@ function handle_request() {
     $action = isset($_GET['action']) ? $_GET['action'] : 'home';
     switch ($action) {
         case 'list':
-            $data = get_data();
-            require_once __DIR__ . '/../views/postit_list.view.php';
+            $userid = get_data($_SESSION);
+            // si récupération du numéro de session en cours non nul (utilisateur connecté) -> OK
+            // je ne vérifie pas si utilisateur dans la bdd, car déjà fait lors de la connexion.
+            if (isset($userid)) {
+                require_once __DIR__ . '/../views/postit_details.view.php';
+            }
+            else {
+                echo "Erreur lors de la récupération des données. Pas d'utilisateur connecté.";
+            }
             break;
         
         case 'details':
